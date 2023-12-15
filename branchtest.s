2c40ee1a404e7b9ab669f1d564272e8ab6f14fd5
Jackson Isenhower <jacksonisenhower@cedarville.edu>
2023-12-15 09:58:09 -0500

    branch test added

---
+++ branchtest.s
@@ -0,0 +1,24 @@
+Begin Assembly
+LABEL Begin
+ADDI R4, R0, 5000
+BNE R4, R5, Begin
+HALT
+End Assembly
+-- begin main data
+Begin Data 4000 44
+10
+0
+23
+71
+33
+5
+93
+82
+34
+13
+111
+23
+End Data
+-- stack
+Begin Data 5000 100
+End Data
